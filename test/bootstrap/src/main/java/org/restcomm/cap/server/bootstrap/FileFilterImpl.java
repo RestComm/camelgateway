2f3c49d55047f809ef1d21e4b5464b10d0f92007
sergey vetyutnev <serg.vetyutnev@gmail.com>
2016-12-28 12:08:34 +0100

    Changing of files with bad packet names

--- test/bootstrap/src/main/java/org/restcomm/cap/server/bootstrap/FileFilterImpl.java
+++ test/bootstrap/src/main/java/org/restcomm/cap/server/bootstrap/FileFilterImpl.java
@@ -20,7 +20,7 @@
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
 
-package org.mobicents.cap.server.bootstrap;
+package org.restcomm.cap.server.bootstrap;
 
 import java.io.File;
 import java.io.FileFilter;
--- test/bootstrap/src/main/java/org/restcomm/cap/server/bootstrap/TestVersion.java
+++ test/bootstrap/src/main/java/org/restcomm/cap/server/bootstrap/TestVersion.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.mobicents.cap.server.bootstrap;
+package org.restcomm.cap.server.bootstrap;
 
 import java.io.InputStream;
 import java.util.Collections;
